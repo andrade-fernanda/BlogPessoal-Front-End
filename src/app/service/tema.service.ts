599f1324ccb68a392bfa992279a04b6cca7fab42
andrade-fernanda <fvw.andrade@gmail.com>
2022-09-11 19:51:20 -0300

    Put e Delete de Tema pt1

--- src/app/service/tema.service.ts
+++ src/app/service/tema.service.ts
@@ -20,8 +20,20 @@ export class TemaService {
     return this.http.get<Tema[]>("http://localhost:8080/temas", this.token) 
   }
 
+  getByIdTema(id:number): Observable<Tema>{
+    return this.http.get<Tema>(`http://localhost:8080/temas/${id}`, this.token)
+  }
+
   postTema(tema: Tema): Observable<Tema>{
     return this.http.post<Tema>("http://localhost:8080/temas", tema, this.token)
   }
+
+  putTema(tema: Tema): Observable<Tema>{
+    return this.http.put<Tema>("http://localhost:8080/temas", tema, this.token)
+  }
   
+  deleteTema(id: number) {
+    return this.http.delete(`http://localhost:8080/temas/${id}`, this.token)
+  }
+
 }
